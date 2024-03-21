94aed32c17ff6fcffc5d22518147a164f3bb8581
Esra-Alhussain <esra.alhussain.s@gmail.com>
2024-03-20 17:28:40 -0700

    Added code to display popular quizzes from the state in the Discovery component

--- src/components/Discovery.jsx
+++ src/components/Discovery.jsx
@@ -3,11 +3,21 @@ import { Link } from 'react-router-dom';
 import playQuiz from "./playQuiz"
 
 const Discovery = ({ quizData }) => {
-    // console.log('quizData:',quizData)
+    console.log('quizData:',quizData)
     return(
         <div>
-            
- 
+            <h2>Popular Quizzes </h2>
+                <div className='popularQuizzes'>
+                {quizData.map((quiz) => (
+                        <div key={quiz.id} className="quiz">
+                        <h3 className="quizTitle">{quiz.name}</h3>
+
+                        <Link to={`playQuiz/${quiz.id}`}>
+                            <button className="playQuiz">Play the Quiz</button>
+                        </Link>
+                        </div>
+                    ))}
+                </div>
         </div>
         
     )
